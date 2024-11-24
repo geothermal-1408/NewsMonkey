9585ecd72df9d2609b5c9c3abbd77d437446adb8
geothermal-1408 <savvy3526@gmail.com>
2024-11-24 12:13:21 +0530

    added

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export default class About extends Component {
-  render() {
-    return (
-      <div >
-        <p className='about'>this is about page</p>
+export default function About() {
+  return (
+    <div className="about-container">
+      <div className="about-header">
+        <h1>News Monkey</h1>
+        <p>know what's going on Earth 🌏</p>
       </div>
-    )
-  }
-}
+      <div className="about-content">
+        <div className="about-section">
+          <p>Go through Different Genres of News and keep updated yourself</p>
+        </div>
 
+        <div className="about-section">
+          <h2>Created By </h2>
+          <div className="team">
+            <div className="team-member">
+              <img src="/newsmonkey_logo.jpg" alt="Team Member" />
+              <h3>Savvy</h3>
+              <p>Noob devloper</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
